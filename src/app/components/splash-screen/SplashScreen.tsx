acbbd79ee5b41ab422a2115bd3e02735b6a713b7
Joop Kiefte <ikojba@gmail.com>
2026-02-28 00:01:25 +0000

    branding: Wally, a Cinny fork

--- src/app/components/splash-screen/SplashScreen.tsx
+++ src/app/components/splash-screen/SplashScreen.tsx
@@ -21,7 +21,7 @@ export function SplashScreen({ children }: SplashScreenProps) {
         justifyContent="Center"
       >
         <Text size="H2" align="Center">
-          Cinny
+          Wally
         </Text>
       </Box>
     </Box>
